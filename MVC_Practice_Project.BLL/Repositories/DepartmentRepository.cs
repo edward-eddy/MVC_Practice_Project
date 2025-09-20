12995b2b92632bfbdcf80d733a45d9c4dcbfac43
edwar <edwardsameh99@gmail.com>
2025-09-20 21:36:53 +0300

    Vid06

---
+++ MVC_Practice_Project.BLL/Repositories/DepartmentRepository.cs
@@ -0,0 +1,50 @@
+﻿using MVC_Practice_Project.BLL.Interfaces;
+using MVC_Practice_Project.DAL.Data.Contexts;
+using MVC_Practice_Project.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC_Practice_Project.BLL.Repositories
+{
+    public class DepartmentRepository : IDepartmentRepository
+    {
+        private readonly AppDbContext _context;
+
+        public DepartmentRepository()
+        {
+            _context = new AppDbContext();
+        }
+
+        public IEnumerable<Department> GetAll()
+        {
+            return _context.Departments.ToList();
+        }
+
+        public Department? Get(int id)
+        {
+            //return context.Departments.FirstOrDefault(D => D.Id == id);
+            return _context.Departments.Find(id);
+        }
+
+        public int Add(Department department)
+        {
+            _context.Departments.Add(department);
+            return _context.SaveChanges();
+        }
+
+        public int Update(Department department)
+        {
+            _context.Departments.Update(department);
+            return _context.SaveChanges();
+        }
+
+        public int Delete(Department department)
+        {
+            _context.Departments.Remove(department);
+            return _context.SaveChanges();
+        }
+    }
+}
