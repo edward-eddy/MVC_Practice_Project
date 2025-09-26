684c6043b786f623201fad26fd3d982673e7c86e
edwar <edwardsameh99@gmail.com>
2025-09-26 14:39:41 +0300

    Vid01 -> Vid04

--- MVC_Practice_Project.PL/Controllers/DepartmentController.cs
+++ MVC_Practice_Project.PL/Controllers/DepartmentController.cs
@@ -46,5 +46,66 @@ namespace MVC_Practice_Project.PL.Controllers
             }
             return View(model);
         }
+        [HttpGet]
+        public IActionResult Details([FromRoute] int? id)
+        {
+            if (id is null) return BadRequest("Invalid Id");
+            var department = _departmentRepository.Get(id.Value);
+            if (department is null) return NotFound(new { statusCode = 404, ErrorMessage = $"Department with Id: {id} not Found" });
+
+            return View(department);
+        }
+        [HttpGet]
+        public IActionResult Edit([FromRoute] int? id)
+        {
+            if (id is null) return BadRequest("Invalid Id");
+            var department = _departmentRepository.Get(id.Value);
+            if (department is null) return NotFound(new { statusCode = 404, ErrorMessage = $"Department with Id: {id} not Found" });
+
+            var dto = new CreateDepartmentDto()
+            {
+                Code = department.Code,
+                Name = department.Name,
+                CreateAt = department.CreateAt,
+            };
+            return View(dto);
+        }
+        [HttpPost]
+        public IActionResult Edit([FromRoute] int id, CreateDepartmentDto model)
+        {
+            if (ModelState.IsValid) // Server Side Validation
+            {
+                var department = new Department()
+                {
+                    Code = model.Code,
+                    Name = model.Name,
+                    CreateAt = model.CreateAt,
+                    Id = id
+                };
+                var Count = _departmentRepository.Update(department);
+                if (Count > 0)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+            if (id is null) return BadRequest("Invalid Id");
+            Department department = _departmentRepository.Get(id.Value);
+            if (department is null) return NotFound();
+
+            var Count = _departmentRepository.Delete(department);
+            if (Count > 0)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+
+            return View("Index");
+        }
     }
 }
--- MVC_Practice_Project.PL/wwwroot/js/site.js
+++ MVC_Practice_Project.PL/wwwroot/js/site.js
@@ -2,3 +2,9 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+const myModal = document.getElementById('myModal')
+const myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', () => {
+    myInput.focus()
+})
