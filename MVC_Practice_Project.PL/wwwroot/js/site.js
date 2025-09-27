838408708941e8551b4c65830d046d97e7e61781
edwar <edwardsameh99@gmail.com>
2025-09-27 20:00:18 +0300

    Vid05

--- MVC_Practice_Project.PL/wwwroot/js/site.js
+++ MVC_Practice_Project.PL/wwwroot/js/site.js
@@ -2,9 +2,3 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-const myModal = document.getElementById('myModal')
-const myInput = document.getElementById('myInput')
-
-myModal.addEventListener('shown.bs.modal', () => {
-    myInput.focus()
-})
