b7b3e03c3f642f85aa94a2bc2b8d78b9799e3bc5
edwar <edwardsameh99@gmail.com>
2025-10-06 01:46:02 +0300

    05 AccountController - SignIn

--- MVC_Practice_Project.PL/Controllers/AccountController.cs
+++ MVC_Practice_Project.PL/Controllers/AccountController.cs
@@ -31,7 +31,7 @@ namespace MVC_Practice_Project.PL.Controllers
                 var user = await _userManager.FindByNameAsync(model.UserName);
                 if (user is null)
                 {
-                    user = await _userManager.FindByNameAsync(model.Email);
+                    user = await _userManager.FindByEmailAsync(model.Email);
                     if (user is null)
                     {
                         user = new AppUser()
@@ -66,6 +66,32 @@ namespace MVC_Practice_Project.PL.Controllers
         #endregion
 
         #region SignIn
+
+        [HttpGet]
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> SignIn(SignInDto model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByEmailAsync(model.Email);
+                if (user is not null)
+                {
+                    var flag = await _userManager.CheckPasswordAsync(user, model.Password);
+                    if (flag)
+                    {
+                        // Sign In
+
+                        return RedirectToAction(nameof(HomeController.Index), "Home");
+                    }
+                }
+                ModelState.AddModelError("", "Invalid SignUp");
+            }
+            return View(model);
+        }
 
         #endregion
 
---
+++ MVC_Practice_Project.PL/DTOs/SignInDto.cs
@@ -0,0 +1,9 @@
+﻿namespace MVC_Practice_Project.PL.DTOs
+{
+    public class SignInDto
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public bool IsRemembered { get; set; }
+    }
+}
